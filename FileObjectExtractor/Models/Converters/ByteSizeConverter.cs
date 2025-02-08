2be0a9a199a95bd58d7a35634d56b2e9e1587087
Peter Callioni <pcallioni@hotmail.com>
2025-02-08 13:57:08 +1000

    Add byste size converter for use in UI.

---
+++ FileObjectExtractor/Models/Converters/ByteSizeConverter.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace FileObjectExtractor.Models.Converters
+{
+    public class ByteSizeConverter : IValueConverter
+    {
+        private static readonly string[] SizeSuffixes = { "B", "KiB", "MiB", "GiB", "TiB" };
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is byte[] byteArray)
+            {
+                double size = byteArray.Length;
+                int order = 0;
+
+                while (size >= 1024 && order < SizeSuffixes.Length - 1)
+                {
+                    order++;
+                    size /= 1024;
+                }
+
+                return string.Format("{0:0.##} {1}", size, SizeSuffixes[order]);
+            }
+
+            return "0 B";
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            Avalonia.Data.BindingNotification state = new Avalonia.Data.BindingNotification(value);
+            state.Error = new NotImplementedException();
+            state.ErrorType = Avalonia.Data.BindingErrorType.Error;
+            return state;
+        }
+    }
+}
---
+++ FileObjectExtractorTests/Models/Converters/ByteSizeConverterTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace FileObjectExtractor.Models.Converters.Tests
+{
+    [TestClass()]
+    public class ByteSizeConverterTests
+    {
+        ByteSizeConverter _converter;
+
+        public ByteSizeConverterTests()
+        {
+            _converter = new ByteSizeConverter();
+        }
+
+        [TestMethod()]
+        public void ConvertTest()
+        {
+            (byte[] byteArray, string expected)[] testCases = new (byte[] byteArray, string expected)[]
+            {
+                (new byte[0], "0 B"),
+                (new byte[512], "512 B"),
+                (new byte[1023], "1023 B"),
+                (new byte[1024], "1 KiB"),
+                (new byte[2048], "2 KiB"),
+                (new byte[1048576], "1 MiB"),
+                (new byte[1073741824], "1 GiB"),
+            };
+
+            foreach ((byte[] byteArray, string expected) in testCases)
+            {
+                // Act
+                object result = _converter.Convert(byteArray, typeof(string), null, CultureInfo.InvariantCulture);
+
+                // Assert
+                Assert.AreEqual(expected, result);
+            }
+        }
+    }
+}
