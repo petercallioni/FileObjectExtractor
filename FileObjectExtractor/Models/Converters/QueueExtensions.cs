d9080d401341075e36f57fdcd1c947b5e5ae2ad3
Peter Callioni <pcallioni@hotmail.com>
2025-02-12 19:59:50 +1000

    Added  initial extraction from bin files.

---
+++ FileObjectExtractor/Models/Converters/QueueExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileObjectExtractor.Models.Converters
+{
+    public static class QueueExtensions
+    {
+        public static T[] DequeueMultiple<T>(this Queue<T> queue, int count)
+        {
+            if (count < 0 || count > queue.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "Count must be non-negative and less than or equal to the number of elements in the queue.");
+            }
+
+            T[] result = new T[count];
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = queue.Dequeue();
+            }
+
+            return result;
+        }
+    }
+}
--- FileObjectExtractor/Models/FileController.cs
+++ FileObjectExtractor/Models/FileController.cs
@@ -1,6 +1,12 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Platform.Storage;
+using FileObjectExtractor.Models.Converters;
+using OpenMcdf;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace FileObjectExtractor.Models
@@ -21,7 +27,7 @@ namespace FileObjectExtractor.Models
                 AllowMultiple = false
             });
 
-            return files.Count >= 1 ? files[0] : null;
+            return files.FirstOrDefault();
         }
 
         public async Task<bool> SaveFileAsync(ExtractedFile extractedFile)
@@ -31,13 +37,103 @@ namespace FileObjectExtractor.Models
             saveOptions.SuggestedFileName = extractedFile.FileName;
             IStorageFile? file = await window.StorageProvider.SaveFilePickerAsync(saveOptions);
 
-            if (file != null)
+            using (MemoryStream memoryStream = new MemoryStream(extractedFile.EmbeddedFile))
             {
-                File.WriteAllBytes(file.Path.AbsolutePath, extractedFile.EmbeddedFile);
-                return true;
+                CompoundFile cf = new CompoundFile(memoryStream);
+                // Iterate through all the streams in the root storage
+                cf.RootStorage.VisitEntries((x) =>
+                {
+                    Console.WriteLine(x.Name);
+                },
+                false);
+
+                if (file != null)
+                {
+                    File.WriteAllBytes(file.Path.AbsolutePath, extractedFile.EmbeddedFile);
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        public byte[] StripHeaderAndTrailer(byte[] inputBin)
+        {
+            byte[] contentArray = [];
+            using (MemoryStream memoryStream = new MemoryStream(inputBin))
+            {
+                CompoundFile cf = new CompoundFile(memoryStream);
+                // Iterate through all the streams in the root storage
+
+                // Iterate through all the entries in the root storage
+                cf.RootStorage.VisitEntries(entry =>
+                {
+                    // Check if the entry is a stream
+                    if (entry.IsStream)
+                    {
+                        // Cast the entry to CFStream
+                        CFStream stream = (CFStream)entry;
+
+                        // Get the stream name
+                        string streamName = stream.Name;
+                        string text = Encoding.UTF8.GetString(stream.GetData());
+                        byte[] bytes = stream.GetData();
+
+                        if (streamName.Equals("CONTENTS"))
+                        {
+                            // Extract the binary data
+                            contentArray = bytes;
+                            return;
+                        }
+                        else if (streamName.Equals("\u0001Ole10Native"))
+                        {
+                            // Parse the Ole10Native data to extract the original data
+                            contentArray = ExtractOle10NativeData(bytes);
+                            return;
+                        }
+                    }
+                }, false); // The second parameter indicates whether to visit sub-storages recursively                
             }
 
-            return false;
+            return contentArray;
+
+        }
+
+        private byte[] ExtractOle10NativeData(byte[] data)
+        {
+            Queue<byte> bytes = new Queue<byte>(data);
+            string text = Encoding.UTF8.GetString(data);
+            List<byte> fileName = new List<byte>();
+            List<byte> sourcePath = new List<byte>();
+
+            bytes.DequeueMultiple<byte>(6);
+            do
+            {
+                fileName.Add(bytes.Dequeue());
+            } while (bytes.First() != 0);
+
+            bytes.Dequeue();
+
+            do
+            {
+                sourcePath.Add(bytes.Dequeue());
+            } while (bytes.First() != 0);
+
+            bytes.Dequeue();
+
+            bytes.DequeueMultiple(4);
+
+            byte[] dwSize = bytes.DequeueMultiple(4);
+
+            uint dwSizeInt = HexConverter.LittleEndianHexToUInt(Convert.ToHexString(dwSize));
+
+            bytes.DequeueMultiple((int)dwSizeInt);
+
+            byte[] dataSize = bytes.DequeueMultiple(4);
+            uint dataSizeInt = HexConverter.LittleEndianHexToUInt(Convert.ToHexString(dataSize));
+            byte[] returnData = bytes.DequeueMultiple((int)dataSizeInt);
+
+            return returnData;
         }
     }
 }
