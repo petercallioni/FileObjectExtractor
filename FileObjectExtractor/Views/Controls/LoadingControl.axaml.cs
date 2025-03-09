061be1b3b57697d488fbc2f2d844dc94abcfdc4f
Peter Callioni <pcallioni@hotmail.com>
2025-03-09 16:05:29 +1000

    Added loading graphic

---
+++ FileObjectExtractor/Views/Controls/LoadingControl.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FileObjectExtractor.Views.Controls;
+
+public partial class LoadingControl : UserControl
+{
+    public LoadingControl()
+    {
+        InitializeComponent();
+    }
+}
